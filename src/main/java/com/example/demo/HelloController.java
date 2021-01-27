b12a0f516520b73e343380ace2c320a2157ba759
r3turnz3ro <khan.salauddin@hotmail.com>
2021-01-27 15:30:26 -0500

    demo project

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+  @GetMapping("/hello")
+  public ResponseEntity<?> hello() {
+    return ResponseEntity.ok("hello");
+  }
+
+}
